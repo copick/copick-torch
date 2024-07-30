0b178c0c890702d7f6795fb85a9bb080579886f7
Kyle Harrington <czi@kyleharrington.com>
2024-07-30 13:57:57 -0400

    Update for mlflow

--- copick_torch/training.py
+++ copick_torch/training.py
@@ -1,38 +1,55 @@
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import MLFlowLogger
+import mlflow.pytorch
 
-def train_model(model, train_loader, val_loader, lr, logdir_path, log_every_n_iterations, val_check_interval, accumulate_grad_batches, model_name="model"):
-    best_checkpoint_callback = ModelCheckpoint(
-        save_top_k=1,
-        monitor="val_loss",
-        mode="min",
-        dirpath=logdir_path,
-        every_n_epochs=1,
-        filename=f"{model_name}-best",
-    )
-    last_checkpoint_callback = ModelCheckpoint(
-        save_top_k=1,
-        save_last=True,
-        dirpath=logdir_path,
-        every_n_epochs=1,
-        filename=f"{model_name}-last",
-    )
+def train_model(model, train_loader, val_loader, lr, logdir_path, log_every_n_iterations, val_check_interval, accumulate_grad_batches, model_name="model", experiment_name="Copick Training"):
+    mlflow.set_experiment(experiment_name)
+    with mlflow.start_run() as run:
+        # Log model parameters
+        mlflow.log_params({
+            "learning_rate": lr,
+            "batch_size": train_loader.batch_size,
+            "val_check_interval": val_check_interval,
+            "accumulate_grad_batches": accumulate_grad_batches,
+            "model_name": model_name
+        })
+        
+        best_checkpoint_callback = ModelCheckpoint(
+            save_top_k=1,
+            monitor="val_loss",
+            mode="min",
+            dirpath=logdir_path,
+            every_n_epochs=1,
+            filename=f"{model_name}-best",
+        )
+        last_checkpoint_callback = ModelCheckpoint(
+            save_top_k=1,
+            save_last=True,
+            dirpath=logdir_path,
+            every_n_epochs=1,
+            filename=f"{model_name}-last",
+        )
 
-    learning_rate_monitor = LearningRateMonitor(logging_interval="step")
+        learning_rate_monitor = LearningRateMonitor(logging_interval="step")
 
-    logger = TensorBoardLogger(save_dir=logdir_path, name="lightning_logs")
+        logger = MLFlowLogger(experiment_name=experiment_name, run_name=model_name)
 
-    trainer = pl.Trainer(
-        accelerator="gpu",
-        devices=1,
-        callbacks=[best_checkpoint_callback, last_checkpoint_callback, learning_rate_monitor],
-        logger=logger,
-        max_epochs=10000,
-        accumulate_grad_batches=accumulate_grad_batches,
-        log_every_n_steps=log_every_n_iterations,
-        val_check_interval=val_check_interval,
-        check_val_every_n_epoch=6,
-    )
-    trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+        trainer = pl.Trainer(
+            accelerator="gpu",
+            devices=1,
+            callbacks=[best_checkpoint_callback, last_checkpoint_callback, learning_rate_monitor],
+            logger=logger,
+            max_epochs=10000,
+            accumulate_grad_batches=accumulate_grad_batches,
+            log_every_n_steps=log_every_n_iterations,
+            val_check_interval=val_check_interval,
+            check_val_every_n_epoch=6,
+        )
+        trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+        
+        # Log the best model
+        best_model_path = best_checkpoint_callback.best_model_path
+        mlflow.pytorch.log_model(model, "best_model", registered_model_name=model_name)
+        mlflow.log_artifact(best_model_path)
