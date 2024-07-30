74f36cf04cdb7275ba7664f3b725c2314bf65743
Kyle Harrington <czi@kyleharrington.com>
2024-07-30 13:36:06 -0400

    Take model name as input

--- copick_torch/training.py
+++ copick_torch/training.py
@@ -3,21 +3,21 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 
-def train_model(model, train_loader, val_loader, lr, logdir_path, log_every_n_iterations, val_check_interval, accumulate_grad_batches):
+def train_model(model, train_loader, val_loader, lr, logdir_path, log_every_n_iterations, val_check_interval, accumulate_grad_batches, model_name="model"):
     best_checkpoint_callback = ModelCheckpoint(
         save_top_k=1,
         monitor="val_loss",
         mode="min",
         dirpath=logdir_path,
         every_n_epochs=1,
-        filename="model-best",
+        filename=f"{model_name}-best",
     )
     last_checkpoint_callback = ModelCheckpoint(
         save_top_k=1,
         save_last=True,
         dirpath=logdir_path,
         every_n_epochs=1,
-        filename="model-last",
+        filename=f"{model_name}-last",
     )
 
     learning_rate_monitor = LearningRateMonitor(logging_interval="step")
